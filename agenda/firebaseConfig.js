cf2a706f6ac0061ebe550a2de7aa8a98d40659ec
FAeliteairsoft <fateamelite@gmail.com>
2025-07-04 20:01:46 -0300

    Add files via upload

---
+++ agenda/firebaseConfig.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyAmO2s7Ttp85MLLildFjRWholYEMkGokuQ",
+  authDomain: "operacoes-36fdb.firebaseapp.com",
+  projectId: "operacoes-36fdb",
+  storageBucket: "operacoes-36fdb.firebasestorage.app",
+  messagingSenderId: "349013594749",
+  appId: "1:349013594749:web:3a573dd2ace366aed11714",
+  measurementId: "G-BL5S0XVXJ9"
+};
